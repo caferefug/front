b4db4be05a15fa795912bb5c5c64e21f801e0eca
iroha <iroha@example.com>
2019-03-17 04:48:23 +0900

    追加

--- js/history.js
+++ js/history.js
@@ -1,27 +1,53 @@
 //http://trunk-hackathon.herokuapp.com/api/{id}/{page}
+// //static/画像名
+const BASE_URL="";
+let windowHeight=0;
+let fstCard=0;
+let endCard=10;
+let cardNum=10;
 $(function(){
-    $('#getImgBtn').on('click',function(){
+    getImg(1,1);
+    //リロード時ajaxで全権取得
+    $(window).bottom({proximity: 0.10});
+    $(window).bind('bottom',function(){
+        var obj=$(this);
+        fstCard+=10;
+        if(cardNum<=10){
+            endCard=cardNum;
+        }else{
+            endCard+=10;
+            cardNum-10;
+        }
         getImg(1,1);
     });
 });
 
 const getImg=function(userId,page){
     $.ajax({
-        url: "http://hogehoge.herokuapp.com/api/1/1",
+        url: "https://trunk-hackathon.herokuapp.com/history.php?page=1&tero_id=1",
         type: "POST",
         dataType: "json"
     }).done((res)=>{
         console.log(res);
-        for(let i=0;i<10;i++){
-            $('.row-result>div').append('<div class="img-result"></div>');
-            $('.img-result').eq(i).show(500);
+        let imgName=res.data;
+        let delayTime=0;
+        const initAplyTime=200;
+        for(let i=fstCard;i<endCard;i++){
+            $('.row-result div[class^="col-"]').append('<div class="img-wrap"><img src='+BASE_URL+imgName+' class="img-result center-content"></div>');
+        }
+        for(let i=fstCard;i<fstCard+10;i++){
+            $('.img-wrap').eq(i).delay(initAplyTime+delayTime).fadeIn(initAplyTime);
+            delayTime+=200;
         }
     }).fail((error)=>{
         let delayTime=0;
-        const initApyTime=300;
-        for(let i=0;i<10;i++){
-            $('.row-result>div').append('<div class="img-wrap"><img src="hondana.jpg" class="img-result center-content"></div>').hide().fadeIn(initApyTime+delayTime);
-            delayTime+=100;
+        const initAplyTime=200;
+        for(let i=fstCard;i<endCard;i++){
+            $('.row-result div[class^="col-"]').append('<div class="img-wrap"><img src='+BASE_URL+' class="img-result center-content"></div>');
+        }
+        for(let i=fstCard;i<endCard;i++){
+            $('.img-wrap').eq(i).delay(initAplyTime+delayTime).fadeIn(initAplyTime);
+            delayTime+=200;
         }
     })
 }
