50e2a124592b174ff5baee563ac06493079195d2
iroha <iroha@example.com>
2019-03-17 06:12:31 +0900

    履歴画面の追加

--- js/history.js
+++ js/history.js
@@ -1,25 +1,34 @@
 //http://trunk-hackathon.herokuapp.com/api/{id}/{page}
 // //static/画像名
-const BASE_URL="";
+const BASE_URL="https://f25c46ee.ngrok.io/static/";
 let windowHeight=0;
 let fstCard=0;
 let endCard=10;
 let cardNum=10;
+let userId=null;
 $(function(){
-    getImg(1,1);
-    //リロード時ajaxで全権取得
-    $(window).bottom({proximity: 0.10});
-    $(window).bind('bottom',function(){
-        var obj=$(this);
-        fstCard+=10;
-        if(cardNum<=10){
-            endCard=cardNum;
-        }else{
-            endCard+=10;
-            cardNum-10;
+    $('#idSendBtn').on('click',function(){
+        userId=Number($('#userId').val());
+        if(userId!=null){
+            $('.row-header').fadeOut();
+            getImg(1,userId);
         }
-        getImg(1,1);
-    });
+    })
+    if(userId!=null){
+        //リロード時ajaxで全権取得
+        $(window).bottom({proximity: 0.10});
+        $(window).bind('bottom',function(){
+            var obj=$(this);
+            fstCard+=10;
+            if(cardNum<=10){
+                endCard=cardNum;
+            }else{
+                endCard+=10;
+                cardNum-10;
+            }
+            getImg(1,userId);
+        });
+    }
 });
 
 const getImg=function(userId,page){
@@ -27,12 +36,12 @@ const getImg=function(userId,page){
         url: "https://trunk-hackathon.herokuapp.com/history.php?page=1&tero_id=1",
         type: "GET",
     }).done((res)=>{
-        console.log(res);
-        let imgName=res.data;
+        let imgName=res;
         let delayTime=0;
         const initAplyTime=200;
+        console.log(BASE_URL+imgName[1].data);
         for(let i=fstCard;i<endCard;i++){
-            $('.row-result div[class^="col-"]').append('<div class="img-wrap"><img src='+BASE_URL+imgName+' class="img-result center-content"></div>');
+            $('.row-result div[class^="col-"]').append('<div class="img-wrap"><img src='+BASE_URL+imgName[i].data+' class="img-result center-content"></div>');
         }
         for(let i=fstCard;i<fstCard+10;i++){
             $('.img-wrap').eq(i).delay(initAplyTime+delayTime).fadeIn(initAplyTime);
